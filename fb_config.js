187722a67d388ff9ae26ec49dead1a7ade35ac36
dacardenasa <diego.cardenasaleg@gmail.com>
2020-05-23 14:21:07 -0500

    Uploading data project

---
+++ fb_config.js
@@ -0,0 +1,36 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9L0MSwZqA60fDm1XX3ZlBklaZrjtYCmE",
+  authDomain: "chat-msg-1a885.firebaseapp.com",
+  databaseURL: "https://chat-msg-1a885.firebaseio.com",
+  projectId: "chat-msg-1a885",
+  storageBucket: "chat-msg-1a885.appspot.com",
+  messagingSenderId: "503844125591",
+  appId: "1:503844125591:web:52b956fc24f31244ee22d2",
+  measurementId: "G-1KCHYD1SBC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+var messages = firebase.database().ref('messages');
+messages.on('child_added', function(data){
+  $('#messages').append(`<li id="${ data.key }">${ data.val().body }</li>`);
+})
+
+messages.on('child_removed', function(data){
+  $('#messages #' + data.key).remove();
+})
+
+$('form').on('submit', function(e){
+  e.preventDefault();
+  messages.push().set({ body: $('#m').val() })
+    .catch(function(err){
+      if (err.code.toLowerCase() === "permission_denied" ) {
+        alert('No has iniciado sesion')
+      } else {
+        alert("Error inesperado: " + err)
+      }
+    }); 
+    $('#m').val('');
+})
